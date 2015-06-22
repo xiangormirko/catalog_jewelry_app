cc5e8226680e55b9c311388d97bd500380d2625c
xiangormirko <xiangormirko@gmail.com>
2015-06-22 16:41:30 +0200

    initial commit

---
+++ vagrant/pg_config.sh
@@ -0,0 +1,24 @@
+apt-get -qqy update
+apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-pip
+pip install bleach
+pip install oauth2client
+pip install requests
+pip install httplib2
+pip install Flask-Login
+pip install Flask
+pip install gunicorn
+pip install Jinja2
+pip install oauth
+pip install oauth2client
+pip install SQLAlchemy
+pip install Werkzeug
+pip install 
+su postgres -c 'createuser -dRS vagrant'
+su vagrant -c 'createdb'
+
+
+vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
+echo -e $vagrantTip > /etc/motd
+
